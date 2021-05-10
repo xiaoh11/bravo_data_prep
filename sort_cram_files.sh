f98a1f20d3d0b3189d9332ce6bd25d2c88f6d873
Colin Gross <grosscol@gmail.com>
2021-05-10 17:50:11 -0400

    Avoid overwritting symlinks

--- sort_cram_files.sh
+++ sort_cram_files.sh
@@ -13,7 +13,7 @@ for i in {0..255}; do printf '%.2x\n' $i; done | xargs mkdir -p
 for f in *.cram; do
   TARGET_DIR=$(sed 's/.cram//' < "$f" | md5sum | cut -c1-2)
   mv "${f}" "${TARGET_DIR}/${f}" 
-  mv "${f}.crai" "${TARGET_DIR}/${f}" 
+  mv "${f}.crai" "${TARGET_DIR}/${f}.crai" 
 done
 
 echo "Done."
