80738e6deac0c9ed29dc2aa462ebe4283ae62bac
Colin Gross <grosscol@gmail.com>
2022-07-14 17:07:14 -0400

    Better encapsulate actual filename structure in test subset.

--- hacks/subset_full_bcf.sh
+++ hacks/subset_full_bcf.sh
@@ -3,29 +3,25 @@
 # Subset full bcf to create testing subset of chr11
 # Use baked in paths from GCP cluster provisioning and freeze 10 bucket.
 
-OUT_DIR=~/bravo-data-subset
-
-SRC_BCF=/mnt/vcfs/freeze10/genotypes/merged/chr11/merged.chr11_5200001_5300000.genotypes.bcf
-OUT_BCF=${OUT_DIR}/chr11.subset.full.bcf
-INTERMEDIATE_BASE=${OUT_DIR}/chr11
+OUT_DIR=~/subset/sampled
+INFILE_LIST=chr11.list.txt
 
 # Make output directory
 mkdir -p ${OUT_DIR}
 
-ARR=(\
-/mnt/vcfs/freeze10/genotypes/merged/chr11/merged.chr11_5200001_5300000.genotypes.bcf \
-/mnt/vcfs/freeze10/genotypes/merged/chr11/merged.chr11_5300001_5400000.genotypes.bcf \
-/mnt/vcfs/freeze10/genotypes/merged/chr11/merged.chr11_5400001_5500000.genotypes.bcf \
-/mnt/vcfs/freeze10/genotypes/merged/chr11/merged.chr11_5500001_5600000.genotypes.bcf )
+# Read list of bcf paths 
+# Example contents:
+# /mnt/vcfs/freeze10/genotypes/merged/chr11/merged.chr11_2600001_2700000.genotypes.bcf
+# /mnt/vcfs/freeze10/genotypes/merged/chr11/merged.chr11_2700001_2800000.genotypes.bcf
+# /mnt/vcfs/freeze10/genotypes/merged/chr11/merged.chr11_2800001_2900000.genotypes.bcf
+
+readarray -t ARR < ${INFILE_LIST}
 
 # Trim BCFS to samples and region
-COUNTER=0
-INTERMEDIATES=()
 for INFILE in "${ARR[@]}"
 do
-  echo "${INFILE}"
-
-  OUTPATH="${INTERMEDIATE_BASE}.part${COUNTER}.bcf"
+  BNAME=$(basename ${INFILE})
+  OUTPATH="${OUT_DIR}/${BNAME}"
 
   # Subset the bcf file
   bcftools view ${INFILE}\
@@ -35,16 +31,5 @@ do
           --samples-file sample_ids.txt \
           --threads 1
 
-  INTERMEDIATES[${COUNTER}]=${OUTPATH}
-  COUNTER=$((COUNTER+1))
+  bcftools index ${OUTPATH} --threads 1
 done
-
-# Consolidate intermediates
-bcftools concat -n -o ${OUT_BCF} --output-type b \
-  "${INTERMEDIATE_BASE}.part0.bcf" \
-  "${INTERMEDIATE_BASE}.part1.bcf" \
-  "${INTERMEDIATE_BASE}.part2.bcf" \
-  "${INTERMEDIATE_BASE}.part3.bcf" 
-
-# Index the subset file
-bcftools index ${OUT_BCF} --threads 1
