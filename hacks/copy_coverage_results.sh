bf7212f25446a0091c52aa0335724e26bb98767a
Colin Gross <grosscol@umich.edu>
2023-02-09 00:30:03 +0000

    Helper script for copying required results

---
+++ hacks/copy_coverage_results.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+# Copy result from coverage workflow to results GCP bucket
+
+SRC=/home/grosscol_umich_edu/data_prep/workflows/coverage/result
+RUN_NAME=fr10_2chrom
+DEST=/mnt/results/${RUN_NAME}
+
+if [ ! -d ${SRC} ]; then
+  echo "Dir not found: ${SRC}"
+  exit 1
+fi
+
+if [ ! -d ${DEST} ]; then
+  mkdir ${DEST}
+fi
+
+# Copy the full coverage result
+echo "Copying full coverage result" 
+cp -Lr ${SRC}/full ${DEST}
+
+# Copy the pruned coverage result
+echo "Copying full coverage result" 
+cp -Lr ${SRC}/bin_* ${DEST}
